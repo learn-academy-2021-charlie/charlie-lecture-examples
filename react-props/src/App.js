b6924390c8b715d9b4782581c7322ca71364f209
WalkerTexas-coder <austinsrwalker@gmail.com>
2021-07-20 14:37:13 -0700

    Props notes

---
+++ react-props/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './App.css'
+import GreetPerson from './components/GreetPerson'
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      namesArray: [
+        "RoboCop",
+        "RickyBobby",
+        "BabaYaga",
+        "BobbyDraper"
+      ],
+      currentPerson: 0,
+    }
+
+  }
+  handleGreeting = () => {
+    let randomNum = Math.floor(Math.random() * this.state.namesArray.length)
+    this.setState({currentPerson: randomNum})
+  }
+  render() {
+    return (
+      <>
+      <GreetPerson 
+      person={this.state.namesArray[this.state.currentPerson]}
+      handleGreeting = {this.handleGreeting}
+      />
+      
+      </>
+    )
+  }
+}
+export default App
